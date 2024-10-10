8b5595f618ecf55bc48a59df93074f6a78ad4fff
Hridyanshu7 <himank7794@gmail.com>
2024-10-10 17:43:25 +0530

    ready for github-Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio-2024/",
+  base: "/portfolio-gh/",
   plugins: [react(), eslint()],
 });
